459b83db6ad7d3b84e8e81c68a247bd7012736e6
yuanyeex <77927412+yuanyeex@users.noreply.github.com>
2024-06-17 00:07:01 +0800

    feat: serial data define and parser interface (#2)

---
+++ herofitness-backend/src/main/java/com/yuanyeex/herofitness/core/model/LatitudeAndLongitude.java
@@ -0,0 +1,9 @@
+package com.yuanyeex.herofitness.core.model;
+
+import lombok.Data;
+
+@Data
+public class LatitudeAndLongitude {
+    private double latitude;
+    private double longitude;
+}
---
+++ herofitness-backend/src/main/java/com/yuanyeex/herofitness/core/service/ActivityParser.java
@@ -0,0 +1,11 @@
+package com.yuanyeex.herofitness.core.service;
+
+public interface ActivityParser {
+
+    /**
+     * Parse activity from local file
+     *
+     * @param localFilePath the local file path
+     */
+    void parse(String localFilePath);
+}
